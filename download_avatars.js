121fa41a0a5b83753dbb97e1eae7622d9f52d4b3
weihansu <weihan.sw@gmail.com>
2019-02-11 22:28:19 +0000

    implement function to download avatar's images

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const secrets = require('./secrets.js');
+const fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,13 +9,11 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': secrets.user_agent,
-      'auth': {
-        'bearer': "bearer " + secrets.token
-      }
+      'Authorization': "token " + secrets.token
     }
   };
+  console.log(options);
   request(options, cb);
-
 }
 
 function handleData(error, response, body) {
@@ -22,11 +21,36 @@ function handleData(error, response, body) {
   console.log('statusCode:', response && response.statusCode);
 
   let parseBody = JSON.parse(body);
+
   parseBody.forEach( function(element, index) {
-    console.log('body: ', element.avatar_url);
+    let pathName = 'avatars/' + element.login;
+    let downloadUrl = element.avatar_url
+    downloadImageByURL(downloadUrl, pathName)
+    // console.log(`User ${element.login} image download complete in path ${pathName}`)
   });
 };
 
-
 getRepoContributors('jquery', 'jquery', handleData);
+
+function downloadImageByURL(url, filePath) {
+  // fs.createWriteStream(filePath);
+  let currentPath = filePath.split('/')[0];
+  let user = filePath.split('/')[1];
+
+  if (!fs.existsSync(currentPath)){
+        fs.mkdirSync(currentPath);
+      }
+
+  let downloder = fs.createWriteStream(filePath);
+
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log(`Downloading image of user ${user}`)
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(downloder);
+}
 
